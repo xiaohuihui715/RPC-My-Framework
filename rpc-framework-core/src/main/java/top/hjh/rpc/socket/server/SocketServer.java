bbdec903bd3492682cc739388e7750920f127a36
hjh <hjh0715123@163.com>
2023-07-03 15:29:49 +0800

    version-2.2 代码优化，使用common保重自定义的ThreadPoolFactory创建Socket版本的线程池

--- rpc-framework-core/src/main/java/top/hjh/rpc/socket/server/SocketServer.java
+++ rpc-framework-core/src/main/java/top/hjh/rpc/socket/server/SocketServer.java
@@ -8,6 +8,7 @@ import top.hjh.rpc.enumeration.RpcError;
 import top.hjh.rpc.exception.RpcException;
 import top.hjh.rpc.registry.ServiceRegistry;
 import top.hjh.rpc.serializer.CommonSerializer;
+import top.hjh.rpc.util.ThreadPoolFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -32,9 +33,7 @@ public class SocketServer implements RpcServer {
 
     public SocketServer(ServiceRegistry serviceRegistry) {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("socket-rpc-server");
     }
 
     @Override
